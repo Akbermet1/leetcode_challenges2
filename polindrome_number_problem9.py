bf295b05b43717cfc8b7abbf0b3f3ee09729c88e
Akbermet Zhuma <akbermet.zh@gmail.com>
2022-02-02 23:26:52 +0600

    version 3, this one is interpreted by python3

--- polindrome_number_problem9.py
+++ polindrome_number_problem9.py
@@ -1,9 +1,19 @@
 # class Solution:
 def isPalindrome(x):
-    string = str(x)
-    string_reversed = string[::-1]
-    return string == string_reversed
+    if x < 0 or (x % 10 == 0 and x != 0): # excluding all numbers ending with 0, except 0
+        return False
 
+    second_half = 0
+
+    while(second_half < x):
+        second_half = (x % 10) + (second_half * 10)
+        if isinstance(x / 10, int): # when the first half is in the range [0,9], division would return a floating point number 
+            break
+        x //= 10
+    return x == second_half or x == second_half // 10  # when the number has an odd number of digits the middle one can be just thown out after calculating teh second
+
+print(isPalindrome(11))
+print(isPalindrome(121))
 print(isPalindrome(1234))
 print(isPalindrome(-1001))
 print(isPalindrome(1001))
